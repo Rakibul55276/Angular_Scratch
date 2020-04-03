4d17c6caa2728b366f3d5c372e42e564306b4142
Rakibul55276 <irakibul550@gmail.com>
2020-04-04 00:25:01 +0800

    cdd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',//selector goes to up directory index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  //gg
+  //gghh
 })
 
 export class AppComponent {
