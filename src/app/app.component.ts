bfec9c97396f71a77bece252ab0124082c448ded
Rakibul55276 <59458916+edrei-deploy@users.noreply.github.com>
2020-04-04 00:17:06 +0800

    cmt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//selector goes to up directory index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'myselfscrstch';
 }
